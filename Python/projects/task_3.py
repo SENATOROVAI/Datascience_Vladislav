6c312d83c44a14a74268bfd897ce7c9df344e493
vladlead5 <vlad.prog55@gmail.com>
2024-05-02 01:24:52 +0300

    Create task_3.py

---
+++ Python/projects/task_3.py
@@ -0,0 +1,18 @@
+"""Train task"""
+
+string = input().split()
+near = set()
+while string != []:
+    for i in range(len(string)):
+        if string[i] == "зайка":
+            if i == 0:
+                near.add(string[i + 1])
+            elif i == len(string) - 1:
+                near.add(string[i - 1])
+            else:
+                near.add(string[i - 1])
+                near.add(string[i + 1])
+    string = input().split()
+
+for el in near:
+    print(el)
