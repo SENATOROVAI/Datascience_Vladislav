cbbc7caaa0a2e9f293c87e3c679b61a8acf58f67
vladlead5 <vlad.prog55@gmail.com>
2024-05-02 00:32:28 +0300

    Update task_1.py

--- Python/projects/task_1.py
+++ Python/projects/task_1.py
@@ -1,16 +1,16 @@
-# """My task of programming."""
+"""My task of programming."""
 
-# number = int(input())
-# list_word = [input() for _ in range(number)]
-# count = 0
-# for word in list_word:
-#     index = 1
-#     hare = ''
-#     for el in word:
-#         hare += el
-#         index += 1
-#         if 'зайка' in hare:
-#             print(index - 5)
-#             break
-#     if 'зайка' not in hare:
-#         print('Заек нет =(')
+number = int(input())
+list_word = [input() for _ in range(number)]
+count = 0
+for word in list_word:
+    index = 1
+    hare = ""
+    for el in word:
+        hare += el
+        index += 1
+        if "зайка" in hare:
+            print(index - 5)
+            break
+    if "зайка" not in hare:
+        print("Заек нет =(")
