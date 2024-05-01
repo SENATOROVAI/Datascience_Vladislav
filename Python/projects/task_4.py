b4ab676c43280764a7b18b6e123f4cd31f8f8f3c
vladlead5 <vlad.prog55@gmail.com>
2024-05-02 01:28:16 +0300

    Create task_4.py

---
+++ Python/projects/task_4.py
@@ -0,0 +1,19 @@
+"""The streaming input"""
+
+from sys import stdin
+
+text = []
+for line in stdin:
+    text.append(line.rstrip("\n").split())
+
+list_word = []
+for element in text:
+    list_word.extend(element)
+
+result = []
+for word in set(list_word):
+    if word.lower() == word[::-1].lower():
+        result.append(word)
+result.sort()
+for res in result:
+    print(res)
